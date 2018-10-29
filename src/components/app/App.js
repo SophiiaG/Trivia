768a6292c390950eba2bf8db1808179898de596a
Sophiia G <sophiagounani@gmail.com>
2018-10-29 15:24:45 +0100

    new arborescence of trivia project

---
+++ src/components/app/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import HomeContainer from '../../views/Home/HomeContainer';
+import CategoryContainer from '../../views/Category/CategoryContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          Je suis dans mon header
+        </header>
+        <Router>
+          <div>
+            <Route exact path="/" component= {HomeContainer}/>
+            <Route exact path="/categories/:name" component= {CategoryContainer}/>
+          </div>
+        </Router>
+        <footer>
+          Je suis dans mon footer
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/Home/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Home = () => (
+  <section>
+    <h1>Homepage</h1>
+  </section>
+);
+
+export default Home;
