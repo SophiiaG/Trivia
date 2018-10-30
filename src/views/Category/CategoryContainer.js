cb1f0f5fdd9499daa1ffa8d0131a3baa702704ad
Sophiia G <sophiagounani@gmail.com>
2018-10-30 11:54:59 +0100

    chnaged console log of the state category

--- src/views/Category/CategoryContainer.js
+++ src/views/Category/CategoryContainer.js
@@ -7,14 +7,14 @@ class CategoryContainer extends Component {
   state = {
     category: null,
   }
-  async componentDidMount() {
+  async ccomponentDidMount() {
     const data = await api.getCategoryById(this.props.match.params.id);
     this.setState({
       category: data,
     });
   }
   render() {
-    console.log(this.props); //pour connaitre les différentes propriétés disponible 
+    console.log(this.state.category); //pour connaitre les différentes propriétés disponible 
     return (
       <Category
       categoryName={this.props.match.params.id}
