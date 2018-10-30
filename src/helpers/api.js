6301dc4f3328c88dcdab1b3d7f4b4f292ca99f42
Sophiia G <sophiagounani@gmail.com>
2018-10-30 11:51:58 +0100

    async

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,9 +1,18 @@
 class api {
-  getcategoryById(id){
-    // Storage.saveItem(response);
-  }
-  getcategories(count){
-
+  // getcategoryById(id){
+  //   fetch(`http://jservice.io/api/category?id=${id}`).then(resp => {
+  //     resp.json().then(category => {
+  //       console.log(category)
+  //     })
+  //   }).catch(err => {
+  //     console.erro(`problems happend`)
+  //   })
+  //   // Storage.saveItem(response);
+  // }
+  async getCategoryById(id) {
+    const category = await fetch(`http://jservice.io/api/category?id=${id}`);
+    const json = await category.json();
+    return json;
   }
 }
-export default api;
+export default new api;
--- src/views/Category/CategoryContainer.js
+++ src/views/Category/CategoryContainer.js
@@ -4,8 +4,14 @@ import Category from './Category';
 import api from '../../helpers/api';
 
 class CategoryContainer extends Component {
-  componentDidMount() {
-    api.getCategoryById(this.props.match.params.id)
+  state = {
+    category: null,
+  }
+  async componentDidMount() {
+    const data = await api.getCategoryById(this.props.match.params.id);
+    this.setState({
+      category: data,
+    });
   }
   render() {
     console.log(this.props); //pour connaitre les différentes propriétés disponible 
